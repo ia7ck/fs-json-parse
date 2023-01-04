e159b0da9881b9ffc0b908129eea9044600d5a8e
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-01-04 15:41:28 +0900

    json parse
    refs: http://www.quanttec.com/fparsec/tutorial.html#parsing-json

--- JsonParse.Test/Program.fs
+++ JsonParse.Test/Program.fs
@@ -1,2 +1,63 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open JsonParse
+
+let test str expected =
+    match parse str with
+    | Ok(value) when value = expected -> Ok()
+    | Ok(actual) -> Error $"Expected:\n{expected}\nActual:\n{actual}"
+    | Error(message) -> Error $"Parse error: {message}"
+
+[<EntryPoint>]
+let main _ =
+    [ "true", Value.True
+      "false", Value.False
+      "null", Value.Null
+      "-1.23", Value.Number -1.23
+      """ "" """, Value.String ""
+      """ " , " """, Value.String " , "
+      """ "abracadabra" """, Value.String "abracadabra"
+      """ " \" " """, Value.String """ " """
+      """ " \\ " """, Value.String """ \ """
+      """ " / " """, Value.String """ / """
+      """ " \/ " """, Value.String """ / """
+      """ " \b " """, Value.String " \b "
+      """ " \f " """, Value.String " \f "
+      """ " \n " """, Value.String " \n "
+      """ " \r " """, Value.String " \r "
+      """ " \t " """, Value.String " \t "
+      """ "line1\u000aline2" """, Value.String "line1\nline2"
+      """ "あ" """, Value.String "あ"
+      """ "\u3042" """, Value.String "あ"
+      """ "\u3044\u2b50" """, Value.String "い⭐"
+      """ "\uD83C\uDF00" """, Value.String "🌀"
+      """ "\u26C4\uD83C\uDF00\u23F3" """, Value.String "⛄🌀⏳"
+      "{}", Value.Object(Map [])
+      """ { "k" : "v" } """, Value.Object(Map [ "k", Value.String "v" ])
+      "[]", Value.Array []
+      """ [ -1.23 , "abracadabra" , { "\ud83d\udd11" : null } , []] """,
+      Value.Array
+          [ Value.Number -1.23
+            Value.String "abracadabra"
+            Value.Object(Map [ "🔑", Value.Null ])
+            Value.Array [] ]
+      "true // comment", Value.True
+      """ "comment//inside//string" """, Value.String "comment//inside//string"
+      """[
+        true, // c1
+        // c2
+        false // c3
+        , // c4
+        null
+      ]""",
+      Value.Array [ Value.True; Value.False; Value.Null ] ]
+    |> List.indexed
+    |> List.choose (fun (i, (input, expected)) ->
+        (input, expected)
+        ||> test
+        |> function
+            | Ok(()) -> None
+            | Error(err) -> Some($"testcase #{i + 1}\n{err}"))
+    |> function
+        | [] -> 0
+        | errors ->
+            printfn "%s" (String.concat "\n\n" errors)
+            1
--- JsonParse/Library.fs
+++ JsonParse/Library.fs
@@ -1,5 +1,74 @@
-﻿namespace JsonParse
+﻿module JsonParse
 
-module Say =
-    let hello name =
-        printfn "Hello %s" name
+open System
+open FParsec
+
+
+[<RequireQualifiedAccessAttribute>]
+type Value =
+    | True
+    | False
+    | Null
+    | Object of Map<string, Value>
+    | Array of Value list
+    | Number of float
+    | String of string
+
+module private Parser =
+    let skipComment = pstring "//" >>. skipRestOfLine false
+    let skip = skipMany (spaces1 <|> skipComment)
+
+    let pEscape =
+        choice
+            [ stringReturn "\\\"" "\"" // \" -> "
+              stringReturn """\\""" """\"""
+              stringReturn """\/""" "/"
+              stringReturn """\b""" "\b"
+              stringReturn """\f""" "\f"
+              stringReturn """\n""" "\n"
+              stringReturn """\r""" "\r"
+              stringReturn """\t""" "\t" ]
+
+    let pUnicodeEscape =
+        pstring """\u""" >>. tuple4 hex hex hex hex
+        |>> (fun (h3, h2, h1, h0) -> Convert.ToUInt16($"{h3}{h2}{h1}{h0}", 16) |> char |> string)
+
+    let pStringLiteral =
+        (pchar '"')
+        >>. manyStrings ((noneOf [ '"'; '\\' ] |>> string) <|> pEscape <|> pUnicodeEscape)
+        .>> (pchar '"')
+
+    let pJsonValue, pJsonValueRef = createParserForwardedToRef<Value, unit> ()
+
+    let pJsonTrue = stringReturn "true" Value.True
+    let pJsonFalse = stringReturn "false" Value.False
+    let pJsonNull = stringReturn "null" Value.Null
+
+    let pKeyValue = pStringLiteral .>>. (skip >>. (pchar ':') >>. skip >>. pJsonValue)
+
+    let pJsonObject =
+        (pchar '{') >>. sepBy (skip >>. pKeyValue .>> skip) (pchar ',') .>> (pchar '}')
+        |>> Map.ofList
+        |>> Value.Object
+
+    let pJsonArray =
+        (pchar '[') >>. sepBy (skip >>. pJsonValue .>> skip) (pchar ',') .>> (pchar ']')
+        |>> Value.Array
+
+    let pJsonNumber = pfloat |>> Value.Number
+    let pJsonString = pStringLiteral |>> Value.String
+
+    pJsonValueRef.Value <-
+        choice
+            [ pJsonTrue
+              pJsonFalse
+              pJsonObject
+              pJsonNull
+              pJsonArray
+              pJsonNumber
+              pJsonString ]
+
+let parse str =
+    match run (Parser.skip >>. Parser.pJsonValue .>> Parser.skip .>> eof) str with
+    | Success(value, _, _) -> Result.Ok value
+    | Failure(message, _, _) -> Result.Error message
