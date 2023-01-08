f2b3d91ccd90ac8d8907cff32f799ac5f3dfc4cd
ia7ck <23146842+ia7ck@users.noreply.github.com>
2023-01-08 22:31:25 +0900

    reduce indent

--- JsonParse/Library.fs
+++ JsonParse/Library.fs
@@ -14,61 +14,61 @@ type Value =
     | Number of float
     | String of string
 
-module private Parser =
-    let skipComment = pstring "//" >>. skipRestOfLine false
-    let skip = skipMany (spaces1 <|> skipComment)
+let private skipComment = pstring "//" >>. skipRestOfLine false
+let private skip = skipMany (spaces1 <|> skipComment)
 
-    let pEscape =
-        choice
-            [ stringReturn "\\\"" "\"" // \" -> "
-              stringReturn """\\""" """\"""
-              stringReturn """\/""" "/"
-              stringReturn """\b""" "\b"
-              stringReturn """\f""" "\f"
-              stringReturn """\n""" "\n"
-              stringReturn """\r""" "\r"
-              stringReturn """\t""" "\t" ]
+let private pEscape =
+    choice
+        [ stringReturn "\\\"" "\"" // \" -> "
+          stringReturn """\\""" """\"""
+          stringReturn """\/""" "/"
+          stringReturn """\b""" "\b"
+          stringReturn """\f""" "\f"
+          stringReturn """\n""" "\n"
+          stringReturn """\r""" "\r"
+          stringReturn """\t""" "\t" ]
 
-    let pUnicodeEscape =
-        pstring """\u""" >>. tuple4 hex hex hex hex
-        |>> (fun (h3, h2, h1, h0) -> Convert.ToUInt16($"{h3}{h2}{h1}{h0}", 16) |> char |> string)
+let private pUnicodeEscape =
+    pstring """\u""" >>. tuple4 hex hex hex hex
+    |>> (fun (h3, h2, h1, h0) -> Convert.ToUInt16($"{h3}{h2}{h1}{h0}", 16) |> char |> string)
 
-    let pStringLiteral =
-        (pchar '"')
-        >>. manyStrings ((noneOf [ '"'; '\\' ] |>> string) <|> pEscape <|> pUnicodeEscape)
-        .>> (pchar '"')
+let private pStringLiteral =
+    (pchar '"')
+    >>. manyStrings ((noneOf [ '"'; '\\' ] |>> string) <|> pEscape <|> pUnicodeEscape)
+    .>> (pchar '"')
 
-    let pJsonValue, pJsonValueRef = createParserForwardedToRef<Value, unit> ()
+let private pJsonValue, pJsonValueRef = createParserForwardedToRef<Value, unit> ()
 
-    let pJsonTrue = stringReturn "true" Value.True
-    let pJsonFalse = stringReturn "false" Value.False
-    let pJsonNull = stringReturn "null" Value.Null
+let private pJsonTrue = stringReturn "true" Value.True
+let private pJsonFalse = stringReturn "false" Value.False
+let private pJsonNull = stringReturn "null" Value.Null
 
-    let pKeyValue = pStringLiteral .>>. (skip >>. (pchar ':') >>. skip >>. pJsonValue)
+let private pKeyValue =
+    pStringLiteral .>>. (skip >>. (pchar ':') >>. skip >>. pJsonValue)
 
-    let pJsonObject =
-        (pchar '{') >>. sepBy (skip >>. pKeyValue .>> skip) (pchar ',') .>> (pchar '}')
-        |>> Map.ofList
-        |>> Value.Object
+let private pJsonObject =
+    (pchar '{') >>. sepBy (skip >>. pKeyValue .>> skip) (pchar ',') .>> (pchar '}')
+    |>> Map.ofList
+    |>> Value.Object
 
-    let pJsonArray =
-        (pchar '[') >>. sepBy (skip >>. pJsonValue .>> skip) (pchar ',') .>> (pchar ']')
-        |>> Value.Array
+let private pJsonArray =
+    (pchar '[') >>. sepBy (skip >>. pJsonValue .>> skip) (pchar ',') .>> (pchar ']')
+    |>> Value.Array
 
-    let pJsonNumber = pfloat |>> Value.Number
-    let pJsonString = pStringLiteral |>> Value.String
+let private pJsonNumber = pfloat |>> Value.Number
+let private pJsonString = pStringLiteral |>> Value.String
 
-    pJsonValueRef.Value <-
-        choice
-            [ pJsonTrue
-              pJsonFalse
-              pJsonObject
-              pJsonNull
-              pJsonArray
-              pJsonNumber
-              pJsonString ]
+pJsonValueRef.Value <-
+    choice
+        [ pJsonTrue
+          pJsonFalse
+          pJsonObject
+          pJsonNull
+          pJsonArray
+          pJsonNumber
+          pJsonString ]
 
 let parse str =
-    match run (Parser.skip >>. Parser.pJsonValue .>> Parser.skip .>> eof) str with
+    match run (skip >>. pJsonValue .>> skip .>> eof) str with
     | Success(value, _, _) -> Result.Ok value
     | Failure(message, _, _) -> Result.Error message
